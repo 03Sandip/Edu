9d06166e6a0a720b514a247bf5c0fc19760ea675
Sandip Paul <paulsandip821@gmail.com>
2025-07-04 13:33:50 +0530

    first commit

--- client_side/edu_teacher/lib/utils/constants.dart
+++ client_side/edu_teacher/lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String uri = 'http://192.168.1.4:3000/api';
+  static const String uri = 'http://192.168.1.2:3000/api';
   //static const String uri = 'http://192.168.76.148:3000/api';
 }
